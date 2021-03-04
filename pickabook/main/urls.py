822ca4d5bb4bd0a1d4a8082eaf181a5a0120795c
Mikhail Onuchin <43931801+MikhailOnuchin@users.noreply.github.com>
2021-03-04 13:44:01 +0300

    auth urls

--- pickabook/main/urls.py
+++ pickabook/main/urls.py
@@ -1,8 +1,30 @@
 from django.urls import path, re_path
 from . import views
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 
 urlpatterns = [
     path('', views.main_page, name='main'),
     path('top/<int:top_id>', views.top_page, name='top_url'),
-    path('book/<int:book_id>', views.book_page, name='book_url')
+    path('book/<int:book_id>', views.book_page, name='book_url'),
+    path('test', views.test)
 ]
+
+authentication_patterns = [
+    path('login', auth_views.LoginView.as_view(template_name='main/login_form.html', form_class=LoginForm,
+                                               redirect_authenticated_user=True), name='login'),
+    path('do-logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('register', views.RegistrationView.as_view(), name='register'),
+]
+
+urlpatterns += authentication_patterns
+'''
+path('reset', auth_views.PasswordResetView.as_view(template_name='main/password_reset_form.html', )),
+path('done', auth_views.PasswordResetDoneView.as_view(template_name='main/password_reset_done.html'),
+     name='password_reset_done'),
+re_path(r'^confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(success_url='/main',
+                                                    template_name='main/password_reset_confirm.html'),
+        name='password_reset_confirm'),
+'''
+
