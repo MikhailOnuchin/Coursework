714547a9acc55703c0387d288cc5838b9e4f5307
Mikhail Onuchin <43931801+MikhailOnuchin@users.noreply.github.com>
2021-06-01 22:48:15 +0300

    fix commit

--- pickabook/main/models.py
+++ pickabook/main/models.py
@@ -29,20 +29,10 @@ class UserBookOperator:
             return 1
 
 
-class Category(models.Model):
-    objects = models.Manager()
-
-    name = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Tag(models.Model):
     objects = models.Manager()
 
     name = models.CharField(max_length=100, unique=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
